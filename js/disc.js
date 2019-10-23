76a40b7b9d029e16f68dcf77dc8f15db8628c7f0
George Antonov <guljash.main@gmail.com>
2019-10-23 13:53:33 +0300

    v3.0

--- js/disc.js
+++ js/disc.js
@@ -32,3 +32,33 @@ document.getElementById('right_main_button').onclick = function(){
 		document.getElementById('wfc').style = 'opacity:0;transition:0.8s;';
 	}
 };
+
+document.getElementById('l_bot_main_button').onclick = function(){
+	'use strict';
+	if (dl === 0){
+		dl = 1;
+		document.getElementById('l_bot_sub_button').style = 'height:33vh;';	
+		document.getElementById('scan_a4a3').style = 'opacity:1;transition:1.7s;';	
+		document.getElementById('scan_a4a3_bw').style = 'opacity:1;transition:1.7s;';			
+	}
+	else{
+		dl = 0
+		document.getElementById('l_bot_sub_button').style = 'height:0vh;';
+		document.getElementById('scan_a4a3').style = 'opacity:0;transition:0.5s;';
+		document.getElementById('scan_a4a3_bw').style = 'opacity:0;transition:0.5s;';			
+	}
+};
+
+document.getElementById('r_bot_main_button').onclick = function(){
+	'use strict';
+	if (dl === 0){
+		dl = 1;
+		document.getElementById('r_bot_sub_button').style = 'height:33vh;';	
+		document.getElementById('vis_card').style = 'opacity:1;transition:1.7s;';			
+	}
+	else{
+		dl = 0
+		document.getElementById('r_bot_sub_button').style = 'height:0vh;';
+		document.getElementById('vis_card').style = 'opacity:0;transition:0.5s;';	
+	}
+};
